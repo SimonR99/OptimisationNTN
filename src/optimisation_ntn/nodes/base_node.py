878ddf805f76905181fa18109840da81ab88aece
SimonR99 <simonroy99@hotmail.ca>
2024-10-30 22:57:21 -0400

    fix typo

--- src/optimisation_ntn/nodes/base_node.py
+++ src/optimisation_ntn/nodes/base_node.py
@@ -5,9 +5,9 @@ from ..utils.type import Position
 
 class BaseNode(ABC):
 
-    def __init__(self, node_id: int, initia_position: Position | None = None):
+    def __init__(self, node_id: int, initial_position: Position | None = None):
         self.node_id = node_id
-        self.position = initia_position
+        self.position = initial_position
         self.state = "on"
 
     def turn_on(self):
