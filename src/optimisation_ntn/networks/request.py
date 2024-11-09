e10c7e21292e92ed11846439c4e767e8e0f2acea
SimonR99 <simonroy99@hotmail.ca>
2024-11-08 19:57:58 -0500

    refactor request

--- src/optimisation_ntn/networks/request.py
+++ src/optimisation_ntn/networks/request.py
@@ -1,58 +1,69 @@
-from enum import Enum
 import random
+from enum import Enum
+
 
 class RequestStatus(Enum):
-    IN_TRANSIT = 0
-    IN_PROCESSING_QUEUE = 1
-    PROCESSING = 2
-    COMPLETED = 3
+    CREATED = 0
+    IN_TRANSIT = 1
+    IN_PROCESSING_QUEUE = 2
+    PROCESSING = 3
+    COMPLETED = 4
+
+
+class Priority(Enum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
 
 class Request:
-
     lamda = 0
     size = 0
     cycle_bits = 0
     id = 0
-    priority = random.randint(1,3)
+    priority = random.randint(1, 3)
 
-    def __init__(self, tick: int, target_node : "BaseNode"):
-        self.tick = tick # time of appearance
-        self.satisfaction = False
+    def __init__(self, tick: int, target_node: "BaseNode"):
+        self.id = 0
+        self.tick = tick  # time of appearance
+
+        self.current_node = None
         self.target_node = target_node
+        self.status = RequestStatus.CREATED
+        self.satisfaction = False
+
+        self.qos_limit = 0.0
         self.set_priority_type(self.priority)
 
-    def set_id(self, row:int, col:int):
+    def set_id(self, row: int, col: int):
         self.id = int(f"{row}{col}")
 
     def get_id(self):
         return self.id
 
-    def set_size(self, size:int):
+    def set_size(self, size: int):
         self.size = size
 
-    def set_priority_type (self, priority):
+    def set_priority_type(self, priority):
         match priority:
-            case 1:
+            case Priority.HIGH:
                 self.lamda = 0.2
+                self.qos_limit = 0.1  # 100 ms
                 self.size = random.randint(1, 3)
-                self.cycle_bits = random.randint(100,  130)
-            case 2:
+                self.cycle_bits = random.randint(100, 130)
+            case Priority.MEDIUM:
                 self.lamda = 0.5
+                self.qos_limit = 0.3  # 300 ms
                 self.size = random.randint(4, 6)
-                self.cycle_bits = random.randint(131,  160)
-            case 3:
+                self.cycle_bits = random.randint(131, 160)
+            case Priority.LOW:
                 self.lamda = 1
+                self.qos_limit = 0.5  # 500 ms
                 self.size = random.randint(7, 10)
-                self.cycle_bits = random.randint(161,  200)
+                self.cycle_bits = random.randint(161, 200)
 
-    def set_satisfied(self):
-        self.satisfaction = True
-
-    def set_unsatisfied(self):
-        self.satisfaction = False
+    def is_satisfied(self):
+        return self.satisfaction
 
     def __str__(self):
         return f"Priority: {self.priority} + \nAppearing time: {self.tick} + \nSatisfaction:{self.satisfaction}"
-
-
-
