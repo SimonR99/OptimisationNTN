541013f62b96698bd6af72815e2d408f55a94b2e
Bruno Reis <brunoreis313@gmail.com>
2024-11-28 22:27:43 -0500

    fix

--- src/optimisation_ntn/nodes/base_station.py
+++ src/optimisation_ntn/nodes/base_station.py
@@ -9,8 +9,9 @@ class BaseStation(BaseNode):
         self.processing_power = 50.0
         self.processing_frequency = 1e9
         self.k_const = 10e-28
-        self.add_antenna("VHF", 1.5)
+        self.add_antenna("VHF", 10)
         self.name = "BS"
+        
 
     def __str__(self):
         return f"Base Station {self.node_id}"
